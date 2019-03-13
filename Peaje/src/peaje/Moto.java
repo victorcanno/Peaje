2e76600150fe89a495214251239fc7ac6c7f0d74
victorcanno <lpdaska@gmail.com>
2019-03-13 07:46:23 -0500

    Desarrollo del codigo terminado
    Se termina el codigo de peaje

---
+++ Peaje/src/peaje/Moto.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package peaje;
+
+/**
+ *
+ * @author s204e3
+ */
+
+
+/*
+ * Clase moto que es hija de la clase vehiculo
+ */
+public class Moto extends Vehiculo{
+
+    public Moto(String placa, String marca) {
+        super(placa, marca, 2000);
+    }
+    
+}
